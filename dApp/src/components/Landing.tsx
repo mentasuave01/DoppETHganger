5eede39df3adc0e5ef96317dde22d246a2229fe7
mentasuave01 <60361959+mentasuave01@users.noreply.github.com>
2024-11-16 22:27:14 +0700

    add animation

--- dApp/src/components/Landing.tsx
+++ dApp/src/components/Landing.tsx
@@ -7,6 +7,7 @@ import { useAccount } from "wagmi";
 import Header from "./Header";
 import DialogMenu from "./DialogMenu";
 import Checking from "./Checking";
+import Collisions from "./Collisions";
 
 const Landing: React.FC = () => {
   const { address } = useAccount();
@@ -26,24 +27,7 @@ const Landing: React.FC = () => {
             <div className="w-full flex justify-center mt-4">
               <img src={areYou} alt="are you" />
             </div>
-            <div id="chainContainers">
-              <div className="w-full flex justify-center mt-2 gap-8">
-                <div id="chainOne" className="dopperChainBadge">
-                  EOA: Base
-                </div>
-                <div id="chainTwo" className="dopperChainBadge">
-                  CA: Mainnet
-                </div>
-              </div>
-              <div className="w-full flex justify-center mt-4 gap-8">
-                <div id="addressOne" className="dopperAddressBadge">
-                  0xb8D98a102b0079B69FFbc760C8d857A31653e56e
-                </div>
-                <div id="addressTwo" className="dopperAddressBadge">
-                  0xb8D98a102b0079B69FFbc760C8d857A31653e56e
-                </div>
-              </div>
-            </div>
+            <Collisions />
             <div className="w-full flex justify-center items-center gap-4">
               <div className="w-fit -mt-4">
                 <DynamicConnectButton />
